e5a1e0d7ba4474500d8c9c15336e5f4dc55237db
Logupriya R <logupriya.9788@gmail.com>
2016-06-06 11:44:13 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main(void) {
+	int a;
+	scanf("%d",&a);
+	if(a>0)
+	{
+		printf("Possitive");
+	}
+	else if(a==0)
+	{
+		printf("Zero");
+	}
+	else if(a<0)
+	{
+		printf("Negative");
+	}
+	// your code goes here
+	return 0;
+}
