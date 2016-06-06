cafd24134778f696c80be8f173bcba404038a3f9
Logupriya R <logupriya.9788@gmail.com>
2016-06-06 15:44:40 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -3,13 +3,21 @@
 int main(void) {
 	int a,reverse=0,m;
 	scanf("%d",&a);
+	int n=a;
 	while(a)
 	{
 		m=a%10;
 		reverse=reverse*10+m;
 		a=a/10;
 	}
-	printf("%d",reverse);
+	if(reverse==n)
+	{
+		printf("palindrome");
+	}
+	else
+	{
+		printf("not a palindrome");
+	}
 	// your code goes here
 	return 0;
 }
