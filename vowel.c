ebb2beb821b769bdfde4bfbdce14b757741f8de1
Logupriya R <logupriya.9788@gmail.com>
2016-06-06 12:01:15 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	char a;
+	scanf("%c",&a);
+	if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u' || a=='A' || a=='E' || a=='I' || a=='O' || a=='U')
+	{
+		printf("Vowel");
+	}
+	else
+	{
+		printf("Consonant");
+	}
+	// your code goes here
+	return 0;
+}
